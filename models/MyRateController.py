c8f91c785e459cc10274cc087b1646b20a0f2a90
Felippe Andrade <4drade@gmail.com>
2024-08-30 17:35:42 -0300

    feat(MyRateController): add a random factor to request intervals
    Randomize delays in request intervals to reduce the likelihood of detection.

--- models/MyRateController.py
+++ models/MyRateController.py
@@ -1,4 +1,5 @@
 import time
+from random import random
 
 from instaloader import InstaloaderContext, RateController
 
@@ -6,8 +7,8 @@ from instaloader import InstaloaderContext, RateController
 class MyRateController(RateController):
     def __init__(self, context: InstaloaderContext):
         super().__init__(context)
-        self._earliest_next_request_time = 5
-        self._iphone_earliest_next_request_time = 5
+        self._earliest_next_request_time = 5 + random()
+        self._iphone_earliest_next_request_time = 5 + random()
 
     def sleep(self, secs):
-        time.sleep(secs + 5)
+        time.sleep(secs + 5 + random())
