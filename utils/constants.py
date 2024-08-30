aaf8b6cf73890c5aae879049937bbbd8ab1f7c68
Felippe Andrade <4drade@gmail.com>
2024-08-30 04:08:39 -0300

    feat(utils): add constants file for default configurations
    Introduce a new `constants.py` file in the `utils` module to define default configuration values, including placeholders for username and password, and paths for download and session directories.

---
+++ utils/constants.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+USERNAME: str = "YOUR-USERNAME-HERE"
+PASSWORD: str = "YOUR-PASSWORD-HERE"
+
+DOWNLOAD_DIRECTORY: Path = Path(__file__).parent.parent / "downloads"
+SESSION_DIRECTORY: Path = Path(__file__).parent.parent / "sessions"
