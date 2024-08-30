d6b3f8ab7fe8f094b3086bbcc5d162f988fa3643
Felippe Andrade <4drade@gmail.com>
2024-08-30 14:22:24 -0300

    fix(MyRateController): remove random delays in favor of fixed intervals
    Replaced `randint` delays with fixed values to ensure consistent request timing.

--- models/MyRateController.py
+++ models/MyRateController.py
@@ -1,5 +1,4 @@
 import time
-from random import randint
 
 from instaloader import InstaloaderContext, RateController
 
@@ -7,8 +6,8 @@ from instaloader import InstaloaderContext, RateController
 class MyRateController(RateController):
     def __init__(self, context: InstaloaderContext):
         super().__init__(context)
-        self._earliest_next_request_time = randint(3, 5)
-        self._iphone_earliest_next_request_time = randint(3, 5)
+        self._earliest_next_request_time = 5
+        self._iphone_earliest_next_request_time = 5
 
     def sleep(self, secs):
-        time.sleep(secs + randint(3, 5))
+        time.sleep(secs + 5)
