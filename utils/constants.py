a10524347a77746331f4d7f13e901852fae98e81
Felippe Andrade <4drade@gmail.com>
2024-08-30 23:30:13 -0300

    feat(constants): add LOG_DIRECTORY constant for log file management
    Added a new `LOG_DIRECTORY` constant to the configuration in `utils/constants.py` to manage log files.

--- utils/constants.py
+++ utils/constants.py
@@ -7,5 +7,7 @@ load_dotenv(Path(__file__).parent.parent / ".env")
 
 USERNAME: str = os.environ["INSTA_USERNAME"]
 PASSWORD: str = os.environ["INSTA_PASSWORD"]
+
 DOWNLOAD_DIRECTORY: Path = Path(__file__).parent.parent / "downloads"
 SESSION_DIRECTORY: Path = Path(__file__).parent.parent / "sessions"
+LOG_DIRECTORY: Path = Path(__file__).parent.parent / "logs"
