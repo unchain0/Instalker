5048702f5d70bae6c8ca8fd58c1fdb43d634b5cc
Felippe Andrade <4drade@gmail.com>
2024-09-01 06:21:20 -0300

    refactor(RateController): replace hardcoded intervals with TIMER constant`
    Simplified time intervals by introducing a TIMER constant, ensuring uniformity and ease of adjustments.

--- models/MyRateController.py
+++ models/MyRateController.py
@@ -1,14 +1,15 @@
 import time
-from random import random
 
 from instaloader import InstaloaderContext, RateController
+
+TIMER = 5
 
 
 class MyRateController(RateController):
     def __init__(self, context: InstaloaderContext):
         super().__init__(context)
-        self._earliest_next_request_time = 5 + random()
-        self._iphone_earliest_next_request_time = 5 + random()
+        self._earliest_next_request_time = TIMER
+        self._iphone_earliest_next_request_time = TIMER
 
     def sleep(self, secs):
-        time.sleep(secs + 5 + random())
+        time.sleep(secs + TIMER)
