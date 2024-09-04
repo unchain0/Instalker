4f2e63a342329735a3aa1960cb4d3496194bdbf5
Felippe Andrade <4drade@gmail.com>
2024-09-04 16:45:35 -0300

    feat(models/MyRateController.py): update sleep method to use time.sleep()
    Update the sleep method in the MyRateController class to use the time.sleep() function instead of the super().sleep() method. This ensures that the sleep time includes the additional random value between 13 and 55 seconds.

--- models/MyRateController.py
+++ models/MyRateController.py
@@ -1,3 +1,4 @@
+import time
 from random import randint
 
 from instaloader import InstaloaderContext, RateController
@@ -9,5 +10,5 @@ class MyRateController(RateController):
         self._earliest_next_request_time = randint(13, 55)
         self._iphone_earliest_next_request_time = randint(13, 55)
 
-    def sleep(self, secs: float):
-        return super().sleep(secs + randint(13, 55))
+    def sleep(self, secs: float) -> None:
+        time.sleep(secs + randint(13, 55))
