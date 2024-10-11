49aac85c555aa1162fe6ab945bb9298434fa10f7
Felippe <4drade@gmail.com>
2024-10-10 22:30:58 -0300

    refactor(main): enhance main function documentation and improve application initialization flow

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 """Initialize logging and run the Instagram class."""
 
+import logging
+from pathlib import Path
+
+from src.classes.image_manager import ImageManager
 from src.classes.instagram import Instagram
 from src.logger_config import setup_logging
 
 
 def main() -> None:
-    """Set up logging and run the Instagram class."""
+    """Initialize and run the application.
+
+    This function performs the following steps:
+    1. Sets up logging for the application.
+    2. Logs the start of the application.
+    3. Initializes the download directory path.
+    4. Creates an instance of ImageManager and removes old images.
+    5. Creates an instance of Instagram and runs the application.
+
+    """
     setup_logging()
-    ig = Instagram()
-    ig.run(remove_old_images=True)
+    logger = logging.getLogger(__name__)
+    logger.info("Starting the application")
+
+    download_directory = Path(__file__).parent / "downloads"
+    image_manager = ImageManager(download_directory)
+    image_manager.remove_old_images()
+
+    instagram = Instagram()
+    instagram.run()
 
 
 if __name__ == "__main__":
